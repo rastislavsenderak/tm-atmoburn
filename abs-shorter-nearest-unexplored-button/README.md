6e9ad18a2cded9411c7b220b9a57c2a50cda36f7
Senderák, Rastislav <rastislav.senderak@bootiq.io>
2026-01-24 16:27:00 +0100

    feature: abs-shorter-nearest-unexplored-button (new script)

---
+++ abs-shorter-nearest-unexplored-button/README.md
@@ -0,0 +1,15 @@
+# AtmoBurn Services - Shorter 'Nearest unexplored system' button
+This is Tampermonkey (https://www.tampermonkey.net/) script for Atmoburn game (https://www.atmoburn.com/).
+
+## What it does
+Make button "Nearest unexplored system" shorter, and yellow, for better orientation.
+
+## How to install
+- You should have Tampermonkey (https://www.tampermonkey.net/) or equivalent
+- Open `abs-shorter-explore-nearest-button.user.js` file and go "Raw" in your browser - Tampermonkey should offer you "Install" button - and thats it.
+
+## How to use
+Open fleet screen and look for yellow button labeled "Nearest". That's it.
+
+## Screenshots
+![Example screenshot](doc/Screenshot1.png)
---
+++ abs-shorter-nearest-unexplored-button/abs-shorter-nearest-unexplored-button.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name           abs-shorter-nearest-unexplored-button
+// @namespace      sk.seko
+// @description    Make button "Nearest unexplored system" shorter and yellow.
+// @updateURL      https://github.com/seko70/tm-atmoburn/raw/refs/heads/main/abs-shorter-nearest-unexplored-button/abs-shorter-nearest-unexplored-button.user.js
+// @downloadURL    https://github.com/seko70/tm-atmoburn/raw/refs/heads/main/abs-shorter-nearest-unexplored-button/abs-shorter-nearest-unexplored-button.user.js
+// @homepageURL    https://github.com/seko70/tm-atmoburn/blob/main/abs-shorter-nearest-unexplored-button/README.md
+// @license        MIT
+// @match          https://*.atmoburn.com/fleet.php*
+// @match          https://*.atmoburn.com/fleet/*
+// @grant          none
+// @version        1.0.1
+// ==/UserScript==
+
+const nearestEl = document.getElementById("exploreNearest");
+if (nearestEl) {
+	nearestEl.setAttribute("value", "Nearest");
+	nearestEl.style.color = 'Yellow';
+	nearestEl.title = "This is shorter name for 'Nearest unexplored system' button";
+}
